cb362290094e2669f4e11582c22cae7e67fbb36d
Jiawei - Nitro <carlisle.j.w.liu@gmail.com>
2025-09-19 11:16:10 +1000

    Incorporate generation command.

--- train.sh
+++ train.sh
@@ -45,14 +45,25 @@
 #                --n_levels=3 \
 #                --depth=32 \
 #                --width=512 \
-#                --batch_size=64 > cifar10_no_aug.log 2>&1 &
+#                --batch_size=128 > cifar10_no_dequantisation_bs_128.log 2>&1 &
 
-python glow.py --evaluate \
-               --restore_file="./results/glow/2025-08-06_12-57-28/checkpoint.pt" \
+# python glow.py --evaluate \
+#                --restore_file="./results/glow/2025-08-07_12-22-23/checkpoint.pt" \
+#                --dataset=cifar10 \
+#                --data_dir="./dataset" \
+#                --width=512\
+#                --depth=32\
+#                --batch_size=32\
+#                --n_levels=3
+#         #        --[options of the saved model: n_levels, depth, width, batch_size]
+
+
+python glow.py --generate \
+               --restore_file="./results/glow/2025-08-07_12-22-23/checkpoint.pt" \
                --dataset=cifar10 \
                --data_dir="./dataset" \
                --width=512\
                --depth=32\
                --batch_size=32\
-               --n_levels=3
-        #        --[options of the saved model: n_levels, depth, width, batch_size]
+               --n_levels=3\
+               --z_std=1.0
